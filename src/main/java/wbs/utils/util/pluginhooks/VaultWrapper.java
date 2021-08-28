a3ce77c275fe25ee17b7233e125be77b65026c8f
Weber588 <idonthavetotellyoumyname@gmail.com>
2021-08-28 14:31:31 +1000

    Added permissions & money formatting to VaultWrapper.

--- src/main/java/wbs/utils/util/pluginhooks/VaultWrapper.java
+++ src/main/java/wbs/utils/util/pluginhooks/VaultWrapper.java
@@ -2,8 +2,10 @@ package wbs.utils.util.pluginhooks;
 
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
+import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import wbs.utils.WbsUtils;
 
@@ -16,6 +18,13 @@ public final class VaultWrapper {
     private VaultWrapper() {}
 
     private static Economy eco;
+    public static boolean isEcoSetup() {
+        return eco != null;
+    }
+    private static Permission perms;
+    public static boolean arePermsSetup() {
+        return perms != null;
+    }
 
     public static boolean isConfigured = false;
 
@@ -24,24 +33,37 @@ public final class VaultWrapper {
 
         if (!PluginHookManager.isVaultInstalled()) return;
 
-        RegisteredServiceProvider<Economy> rsp = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
+        RegisteredServiceProvider<Economy> ecoRSP = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
+        if (ecoRSP == null) {
             WbsUtils.getInstance().logger.warning("Economy failed to establish.");
             return;
+        } else {
+            eco = ecoRSP.getProvider();
         }
-        eco = rsp.getProvider();
+
+        RegisteredServiceProvider<Permission> permsRSP = Bukkit.getServer().getServicesManager().getRegistration(Permission.class);
+        if (permsRSP == null) {
+            WbsUtils.getInstance().logger.warning("Permissions manager failed to establish.");
+            return;
+        } else {
+            perms = permsRSP.getProvider();
+        }
+
         isConfigured = true;
     }
 
     public static boolean hasMoney(OfflinePlayer player, double amount) {
+        if (!isEcoSetup()) return false;
         return eco.has(player, amount);
     }
 
     public static double getMoney(OfflinePlayer player) {
+        if (!isEcoSetup()) return 0;
         return eco.getBalance(player);
     }
 
     public static boolean giveMoney(OfflinePlayer player, double amount) {
+        if (!isEcoSetup()) return false;
         if (amount < 0) {
             return takeMoney(player, Math.abs(amount));
         }
@@ -51,11 +73,42 @@ public final class VaultWrapper {
     }
 
     public static boolean takeMoney(OfflinePlayer player, double amount) {
+        if (!isEcoSetup()) return false;
         if (amount < 0) {
             return giveMoney(player, amount);
         }
 
         EconomyResponse response = eco.withdrawPlayer(player, amount);
         return response.transactionSuccess();
+    }
+
+    public static String formatMoney(double amount) {
+        if (!isEcoSetup()) return amount + "";
+
+        return eco.format(amount);
+    }
+
+    public static String formatMoneyFor(OfflinePlayer player) {
+        if (!isEcoSetup()) return "";
+
+        return eco.format(eco.getBalance(player));
+    }
+
+    public static boolean givePermission(Player player, String permission) {
+        if (!arePermsSetup()) return false;
+
+        return perms.playerAdd(player, permission);
+    }
+
+    public static boolean removePermission(Player player, String permission) {
+        if (!arePermsSetup()) return false;
+
+        return perms.playerRemove(player, permission);
+    }
+
+    public static String getGroup(Player player) {
+        if (!arePermsSetup()) return "";
+
+        return perms.getPrimaryGroup(player);
     }
 }
