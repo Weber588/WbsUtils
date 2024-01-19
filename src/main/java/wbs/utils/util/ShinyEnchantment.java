fc3b92534f76b8c56b915dba7e72614ebe8523b9
Weber588 <idonthavetotellyoumyname@gmail.com>
2024-01-19 14:56:28 +1100

    Added Shiny enchantment for effect only

---
+++ src/main/java/wbs/utils/util/ShinyEnchantment.java
@@ -0,0 +1,61 @@
+package wbs.utils.util;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import wbs.utils.WbsUtils;
+
+@SuppressWarnings("unused")
+public class ShinyEnchantment extends Enchantment {
+    public static final ShinyEnchantment SHINY = new ShinyEnchantment();
+
+    private ShinyEnchantment() {
+        super(new NamespacedKey(WbsUtils.getInstance(), "shiny"));
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "Shiny";
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getStartLevel() {
+        return 0;
+    }
+
+    @SuppressWarnings("deprecation")
+    @NotNull
+    @Override
+    public EnchantmentTarget getItemTarget() {
+        return EnchantmentTarget.ALL;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return false;
+    }
+
+    // Call this cursed, because it should be assumed to not be removable in a grindstone.
+    @Override
+    public boolean isCursed() {
+        return true;
+    }
+
+    @Override
+    public boolean conflictsWith(@NotNull Enchantment enchantment) {
+        return false;
+    }
+
+    @Override
+    public boolean canEnchantItem(@NotNull ItemStack itemStack) {
+        return false;
+    }
+}
