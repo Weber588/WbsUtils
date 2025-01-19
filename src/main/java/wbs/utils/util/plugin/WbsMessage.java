25b5c66c00cc9baf38630a920ba3048509e760ea
Weber588 <idonthavetotellyoumyname@gmail.com>
2025-01-19 14:35:26 +1100

    Added functionality for action bars as well as messages

--- src/main/java/wbs/utils/util/plugin/WbsMessage.java
+++ src/main/java/wbs/utils/util/plugin/WbsMessage.java
@@ -1,5 +1,6 @@
 package wbs.utils.util.plugin;
 
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.JoinConfiguration;
 import org.bukkit.Bukkit;
@@ -9,6 +10,8 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 @SuppressWarnings("unused")
 public class WbsMessage {
@@ -17,22 +20,45 @@ public class WbsMessage {
     public WbsMessage(List<Component> components) {
         this.components = components;
     }
+
     @SafeVarargs
-    public final <T extends CommandSender> WbsMessage send(T... receivers) {
+    public final <T extends CommandSender> WbsMessage send(BiConsumer<CommandSender, Component> sendMethod, T... receivers) {
         List<CommandSender> receiverList = new LinkedList<>(Arrays.asList(receivers));
-        return send(receiverList);
+        return send(sendMethod, receiverList);
     }
 
-    public WbsMessage send(Collection<? extends CommandSender> receivers) {
+    public WbsMessage send(BiConsumer<CommandSender, Component> sendMethod, Collection<? extends CommandSender> receivers) {
         Component component = Component.join(JoinConfiguration.builder().build(), components);
         for (CommandSender sender : receivers) {
-            sender.sendMessage(component);
+            sendMethod.accept(sender, component);
         }
 
         return this;
     }
 
+
+    @SafeVarargs
+    public final <T extends CommandSender> WbsMessage send(T... receivers) {
+        return send(Audience::sendMessage, receivers);
+    }
+
+    public WbsMessage send(Collection<? extends CommandSender> receivers) {
+        return send(Audience::sendMessage, receivers);
+    }
+
+    @SafeVarargs
+    public final <T extends CommandSender> WbsMessage sendActionBar(T... receivers) {
+        return send(Audience::sendActionBar, receivers);
+    }
+
+    public WbsMessage sendActionBar(Collection<? extends CommandSender> receivers) {
+        return send(Audience::sendActionBar, receivers);
+    }
+
     public WbsMessage broadcast() {
         return send(Bukkit.getOnlinePlayers());
+    }
+    public WbsMessage broadcastActionBar() {
+        return sendActionBar(Bukkit.getOnlinePlayers());
     }
 }
