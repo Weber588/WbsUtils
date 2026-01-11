2755a48a41ac91ff699cc891848073a56863a3d1
Weber588 <idonthavetotellyoumyname@gmail.com>
2026-01-11 13:21:04 +1100

    Switched to bounding box selection to ensure accurate radius selection

--- src/main/java/wbs/utils/util/entities/selector/RadiusSelector.java
+++ src/main/java/wbs/utils/util/entities/selector/RadiusSelector.java
@@ -3,6 +3,7 @@ package wbs.utils.util.entities.selector;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
+import org.bukkit.util.BoundingBox;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
@@ -17,16 +18,20 @@ public class RadiusSelector<T extends Entity> extends EntitySelector<T, RadiusSe
     @Override
     protected @NotNull List<T> getSelection(Location loc) {
         World world = Objects.requireNonNull(loc.getWorld());
-        Collection<T> entities = filter(world.getNearbyEntities(loc, range, range, range));
+
+        BoundingBox boundingBox = BoundingBox.of(loc, range / 2, range / 2, range / 2);
+
+        Collection<T> entities = filter(world.getNearbyEntities(boundingBox));
 
         Map<T, Double> selected = new HashMap<>();
 
         for (T check : entities) {
             if (!isValid(check)) continue;
 
-            double distanceSquared = check.getLocation().distanceSquared(loc);
-            if (distanceSquared <= range * range) {
+            Location checkLocation = check.getBoundingBox().intersection(boundingBox).getCenter().toLocation(world);
 
+            double distanceSquared = checkLocation.distanceSquared(loc);
+            if (distanceSquared <= range * range) {
                 selected.put(check, distanceSquared);
             }
         }
